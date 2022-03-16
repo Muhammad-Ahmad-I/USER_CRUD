b8d52190ab2b44013bf57918369cd62ee700670d
Muhammad-Ahmad-I <ahmad.azam@invozone.com>
2022-03-16 12:54:34 +0500

    server unconment

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.use(express.json());
 bodyParser.urlencoded({ extended: true });
 app.use('/api', Route);
 
-// app.listen(5000, (req, res) => {
-//   console.log('server start on port 5000');
-// });
+app.listen(5000, (req, res) => {
+  console.log('server start on port 5000');
+});
